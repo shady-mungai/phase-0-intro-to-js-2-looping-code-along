8b5038b0230609fb1320bbc2a80af50ace74ecba
shady-mungai <shadrackmungai10@gmail.com>
2022-12-13 14:11:06 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+console.log(names.length);
+function writeCards(names) 
+{
+    var arr = [];
+    
+    for(var i=0;i<names.length;i++){
+       arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+  return arr;
+}
+
+function  countDown(x)
+{
+    var arr = [];
+
+  for (var i = x ; i >= 0; i--)
+    { 
+        arr.push(i);
+     }
+
+    for(var i=0;i<arr.length;i++)
+    {
+         console.log(arr[i]);
+    }
+    
+
+}
